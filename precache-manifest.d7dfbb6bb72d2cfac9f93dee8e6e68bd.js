7c88af188b4659b5a09cae1b9d43f3a4600a277c
binodgope30 <30909308+binodgope30@users.noreply.github.com>
2022-11-25 15:11:46 +0530

    darg

---
+++ precache-manifest.d7dfbb6bb72d2cfac9f93dee8e6e68bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b74c8bf7b0fd67c755dadedc96b7d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f94f47fea54dcc16258c",
+    "url": "/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94f47fea54dcc16258c",
+    "url": "/static/js/main.a6df8f07.chunk.js"
+  },
+  {
+    "revision": "dab82071668f2702c64d",
+    "url": "/static/js/runtime-main.ab6486c8.js"
+  }
+]);
